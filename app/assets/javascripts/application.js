95d42a80ebc4e0bf1e64f61c216c5e84b6c685a1
Matt Dobson <mdobson4@gmail.com>
2012-02-26 19:27:42 -0500

    backbone

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,4 +6,14 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require underscore
+//= require backbone
+
+//= require studycache
+
+//= require_tree ../templates
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
 //= require_tree .
