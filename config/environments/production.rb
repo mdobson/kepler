9c7ffa7c85ddfa8e503f93a9aba67aef3a9b0731
Matt Dobson <mdobson4@gmail.com>
2012-02-27 22:45:48 -0500

    mailer stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Studycache::Application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'studycache.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'studycache.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
     :address    => "smtp.sendgrid.net",
     :port       => 25,
