f2c1c4d1657c69d55759a40b0e4dc0fa1b96aa99
Matt Dobson <mdobson4@gmail.com>
2012-02-27 23:12:08 -0500

    asfd

--- app/models/access_control.rb
+++ app/models/access_control.rb
@@ -17,8 +17,8 @@ class AccessControl < ActiveRecord::Base
 	  where("study_id = ?", study_id)
   }
   
-  def notify_user
-    Notification.deliver_study(self)
-  end
+  #def notify_user
+  #  Notification.deliver_study(self)
+  #end
 
 end
