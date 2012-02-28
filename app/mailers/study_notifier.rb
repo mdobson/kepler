da41620ff775bb958cc2e8a3a972c650855fb184
Matt Dobson <mdobson4@gmail.com>
2012-02-27 22:40:20 -0500

    got rid of notifier

--- app/mailers/study_notifier.rb
+++
@@ -1,8 +0,0 @@
-class StudyNotifier < ActionMailer::Base
-  def addition_to_study_notification(accessControl)
-  	recipients 		"#{accessControl.user.email}"
-  	from 			"StudyCache Team"
-  	subject 		"You've been added to #{accessControl.study.title}"
-  	sent_on 		Time.now
-  	body 			{ :email => accessControl.user.email, :study => accessControl.study.title }
-end
