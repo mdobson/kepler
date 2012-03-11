c4e4f4c15d99c3177af2fb0bb4541461346e2a2e
Matthew Dobson <mdobson4@gmail.com>
2012-03-10 21:32:27 -0500

    began API access business

--- app/helpers/form_builder_helper.rb
+++ app/helpers/form_builder_helper.rb
@@ -1,6 +1,42 @@
 module FormBuilderHelper
-	def accessFormApi
-		
+	
+	def uri
+		return "http://[URL_HERE].com/"
 	end
 
+
+	def access_form_api(json_form_data, study_id, method_symbol)
+
+		json_form_object = deserialize_json(json_form_data)
+
+		json_form_object_graph = serialize_json(json_form_object, study_id)
+		
+		self.method(method_symbol).call(json_form_object_graph)
+	end
+
+	def deserialize_json(json_string)
+		form_object = ActiveSupport::JSON.decode(json_string)
+		return form_object
+	end
+
+	def serialize_json(json_object, study_id)
+		form_object_graph = ActiveSupport::JSON.encode({:data => json_form_object, :study_id => study_id})
+		return form_object_graph
+	end
+
+	def create_form(object_graph)
+		#create form
+	end
+
+	def edit_form(object_graph)
+		#edit form
+	end
+
+	def delete_form(object_graph)
+		#delete form
+	end
+
+	def perform_request(uri, object_graph)
+		#perform actual request
+	end
 end
