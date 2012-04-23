3f9af6923b1648af37070432bf19285e74f86407
Matthew Dobson <mdobson4@gmail.com>
2012-04-22 21:18:08 -0400

    Revert "commit"
    This reverts commit 1b0d73ac33bb9d04fb853e62c27717839453ec91.

--- app/models/video_survey.rb
+++
@@ -1,2 +0,0 @@
-class VideoSurvey < ActiveRecord::Base
-end
--- test/unit/video_survey_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class VideoSurveyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
