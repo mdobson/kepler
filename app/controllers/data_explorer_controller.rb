e95bdf9139f30143544ff47664d75dbe5b8fe10e
Matthew Dobson <mdobson4@gmail.com>
2012-04-23 22:56:10 -0400

    dynamic data grid rendering

--- app/controllers/data_explorer_controller.rb
+++ app/controllers/data_explorer_controller.rb
@@ -1,5 +1,6 @@
 class DataExplorerController < ApplicationController
   def index
+  	@form = Form.get_forms_by_study_id(params[:study_id]).first
   end
 
 end
--- app/helpers/data_entry_helper.rb
+++ app/helpers/data_entry_helper.rb
@@ -1,2 +1,12 @@
+require 'json'
+
 module DataEntryHelper
+	def get_data_column(fields)
+		array_of_fields = []
+		fields.each do |field|	
+			hash = {:id=>field.field_name,:name=>field.field_name,:field=>field.field_name}
+			array_of_fields.push(hash)
+		end
+		return array_of_fields.to_json
+	end
 end
