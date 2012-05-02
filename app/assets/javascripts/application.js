b9ad5bc0a8c99508af98f5d7a0e083450942f89b
Matt Dobson <mdobson4@gmail.com>
2012-05-01 22:34:48 -0400

    made progress with backbone

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,4 +10,11 @@
 //= require demosurvey
 //= require underscore
 //= require backbone
+//= require studycache
+//= require_tree ../templates
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
+//= require_tree .
 
