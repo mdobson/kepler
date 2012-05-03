1ca718b32a81e0a60785c2a81b2100fbe3305b24
Matthew Dobson <mdobson4@gmail.com>
2012-05-02 21:13:58 -0400

    fixin the link stuff

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,7 +4,6 @@ class DashboardController < ApplicationController
   	@study = Study.find(params[:study_id])
     @users_in_study = AccessControl.all_users_in_study(params[:study_id])
     @posts = Announcements.posts_by_study(params[:study_id])
-    @subjects = ExampleSubject.retrieve_by_study(params[:study_id])
   end
 
 end
