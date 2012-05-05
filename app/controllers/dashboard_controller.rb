3cf065494cc9ead549f57a00a9434861bcfc38c1
Matthew Dobson <mdobson4@gmail.com>
2012-05-05 14:33:44 -0400

    form references added

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,6 +4,7 @@ class DashboardController < ApplicationController
   	@study = Study.find(params[:study_id])
     @users_in_study = AccessControl.all_users_in_study(params[:study_id])
     @posts = Announcements.posts_by_study(params[:study_id])
+    @forms = Form.get_forms_by_study_id(params[:study_id])
   end
 
 end
