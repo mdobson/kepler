39d509bd82fe0b7cb2c246355a0375b104768703
Matthew Dobson <mdobson4@gmail.com>
2012-05-06 14:08:25 -0400

    All field types render and collect data

--- app/helpers/form_controller_helper.rb
+++ app/helpers/form_controller_helper.rb
@@ -5,10 +5,12 @@ module FormControllerHelper
 				return text_field_tag(field["datapoint"])
 			when "Numeric"
 				return text_field_tag(field["datapoint"])
-			when "Checkbox"
+			when "Bool"
 				return check_box_tag(field["datapoint"])
-			when "Select"
+			when "Dropdown"
 				return select_tag(field["datapoint"], options_for_select([[1],[2],[3]]))
+			when "Long"
+				return text_area_tag(field["datapoint"], nil, :size => "400x8", :style=>"width:461px;")
 		end
 	end
 
