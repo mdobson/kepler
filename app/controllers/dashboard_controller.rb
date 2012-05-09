1eb2a66020052ae07ff573ea599b6760b6b40d07
Matthew Dobson <mdobson4@gmail.com>
2012-05-08 21:15:06 -0400

    Some more dashboard stuff done

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,6 +5,8 @@ class DashboardController < ApplicationController
     @users_in_study = AccessControl.all_users_in_study(params[:study_id])
     @posts = Announcements.posts_by_study(params[:study_id])
     @forms = Form.get_forms_by_study_id(params[:study_id])
+    @data_sets = DataSet.get_ordered_forms_by_study_id(params[:study_id])
+    logger.debug @data_sets.to_json
   	acl = AccessControl.for_user_in_study(current_user.id, params[:study_id]).first
   	if acl.is_admin
   		render "admin"
--- app/models/data_set.rb
+++ app/models/data_set.rb
@@ -5,4 +5,12 @@ class DataSet < ActiveRecord::Base
 	scope :get_data_sets_by_study_id, lambda{|study_id|
 		where("study_id = ?", study_id)
 	}
+
+	scope :get_last_entered_date, lambda{|form_id|
+		where("form_id = ?", form_id).order("updated_at ASC").first
+	}
+
+	scope :get_ordered_forms_by_study_id, lambda{|study_id|
+		select("form_id, count(*) as rec_count").where("study_id = ?", study_id).group("form_id")
+	}
 end
