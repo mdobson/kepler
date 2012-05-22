2fa9034b3ef981e8b1ced52b8e3d330ff7b237c0
Matthew Dobson <mdobson4@gmail.com>
2012-05-21 20:27:10 -0400

    Hoping email was fixed via redeploy to heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  #after_create :notify_user
+  after_create :notify_user
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
