59d1e4d1c68310f59920061441af2ea468650349
Matt Dobson <matt.dobson@paywithglyph.com>
2013-03-26 21:55:47 -0400

    Added change to asset precompilation.

--- config/application.rb
+++ config/application.rb
@@ -45,5 +45,9 @@ module Studycache
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    #fix for error in asset compilation
+    #http://www.simonecarletti.com/blog/2012/02/heroku-and-rails-3-2-assetprecompile-error/
+    config.assets.initialize_on_precompile = false
   end
 end
