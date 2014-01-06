e51a40e2d3f9a981d551ce03f08681e4fe14c423
Matthew Dobson <mdobson4@gmail.com>
2014-01-06 16:40:27 -0500

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Studycache::Application.initialize!
 
-DocRaptor.api_key "Q9e25tYKoJOBc6qSn7Lh"
+DocRaptor.api_key "INSERT_DOC_RAPTOR_KEY"
